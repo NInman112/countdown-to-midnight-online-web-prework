9b4c5da30d820d2e4be5cba4fe4278e740628355
Nick Inman <nick@theoatmeal.com>
2018-11-16 03:15:51 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
+def countdown(n)
+  number = n
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(n)
+  number = n
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  sleep 1
+  number -= 1
+end
+  return "HAPPY NEW YEAR!"
 end
