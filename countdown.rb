c0f2d750c51dfeceea2de42b851d3d705d88a886
Nick Inman <nick@theoatmeal.com>
2018-11-16 03:10:32 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -3,10 +3,18 @@ def countdown
   number = 12
   while number > 0
   puts "#{number} SECOND(S)!"
-  sleep 0.5
   number -= 1
 end
   return "HAPPY NEW YEAR!"
 end
 
+def countdown_with_sleep
+  number = 12
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  sleep 0.5
+  number -= 1
+end
+  return "HAPPY NEW YEAR!"
+end
 countdown
